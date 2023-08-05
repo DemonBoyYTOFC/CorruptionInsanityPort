5df03088c46d30d1728d3b32d6ca9e79e78a79a3
glauber10 <124374135+DemonBoyYTOFC@users.noreply.github.com>
2023-08-05 20:03:23 -0300

    Add files via upload

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1141,8 +1141,12 @@ class PlayState extends MusicBeatState
 		botplayTxt.scrollFactor.set();
 		botplayTxt.borderSize = 1.25;
 		botplayTxt.visible = cpuControlled;
-                add(creditTxt);
-	
+      add(botplayTxt);
+	var creditTxt = new FlxText(876, 648, 348);
+     creditTxt.text = "Ported By\nglauber01";
+    creditTxt.setFormat(Paths.font("vcr.ttf"), 30, FlxColor.WHITE, RIGHT, FlxTextBorderStyle.OUTLINE,FlxColor.BLACK);
+    creditTxt.scrollFactor.set();
+    add(creditTxt);
 		if(ClientPrefs.downScroll) {
 			botplayTxt.y = timeBarBG.y - 78;
 		}
